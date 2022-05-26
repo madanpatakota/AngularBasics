4600f831d244017fd71ebac2237e8a4af644dd8a
MadanMohanReddy <madan.patakota@hotmail.com>
2022-05-26 05:36:59 +0530

    angular predefined directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, ElementRef } from '@angular/core';
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,53 +8,102 @@ import { Component, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  Name = "John smith";
-  Address = "India";
-  CustomerID = "cus123"
 
-  constructor(){
-    
-        setTimeout(() => {
-          this.elementType = "checkbox";
-        }, 2000);
+   isApplyStyle = true; //its come from another area
+
+   subjects = [".net","angular","java"];   //aray of the strings
+
+   //var obj = {ID:1,Name:".net"};
+
+   subjects1 = [{ID:1,Name:".net",Location:"bangonore"},{ID:2,Name:"angular"},{ID:3,Name:"java"}]; //array of the objects
+
+
+  //  for(int i=0;i<10;i++){
+
+  //  }
+   
+   constructor(){
+   
+    // solution 1
+    var myArray = [];
+    if(this.isApplyStyle == true){
+      // iam going to writhe the some logic 
+      myArray.push(1);
+    }
+   else{
+      // else relateid code....
+      myArray.push(2);
+   }
+
+   //ternary opertor
+   var myArray2 = [];
+   this.isApplyStyle==true ?  myArray2.push(1): myArray2.push(2);
+
+
+
+
   }
-
-  getName(){
-    return "from getName method".concat(this.Name);
-  }
-
-
-  elementType= "text";
-
-  evtChangeText(event){
-    console.log("keypress has happend",event);
-  }
-
-  s:string = "John";
-  
-
-  evtLocation(locationref:HTMLInputElement){
-     console.log(locationref.value);
-  }
-
-//   evtLocation(locationref:ElementRef){
-//     console.log(locationref.nativeElement);
-//  }
-
-
-  evtAddressChange(address){
-     this.Address = address;
-     console.log(address);
-  }
-
-
-  // getName(name){
-
-  // }
-
 
 }
+
+
+//=====================================================
+
+// import { Component, ElementRef } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+  
+//   Name = "John smith";
+//   Address = "India";
+//   CustomerID = "cus123"
+
+//   constructor(){
+    
+//         setTimeout(() => {
+//           this.elementType = "checkbox";
+//         }, 2000);
+//   }
+
+//   getName(){
+//     return "from getName method".concat(this.Name);
+//   }
+
+
+//   elementType= "text";
+
+//   evtChangeText(event){
+//     console.log("keypress has happend",event);
+//   }
+
+//   s:string = "John";
+  
+
+//   evtLocation(locationref:HTMLInputElement){
+//      console.log(locationref.value);
+//   }
+
+// //   evtLocation(locationref:ElementRef){
+// //     console.log(locationref.nativeElement);
+// //  }
+
+
+//   evtAddressChange(address){
+//      this.Address = address;
+//      console.log(address);
+//   }
+
+
+//   // getName(name){
+
+//   // }
+
+
+// }
 
 
 // import { Component } from '@angular/core';
